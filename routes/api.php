2461c983d0fa3151b290fa9a9ad1ba0c936b8839
Mirna <mirnarecinos382@gmail.com>
2024-06-01 21:44:25 -0300

    :sparkles: Add new routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Http\Controllers\UsuariosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,23 +9,32 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/prueba', [UsuariosController::class, 'prueba']);
 
-// Obtener todos
-Route::get('/getAll', [UsuariosController::class, 'getAllUser']);
+Route::middleware('auth:sanctum')->group(function() {
 
-// Obtener por id
-Route::get('/user/{id}', [UsuariosController::class, 'usuario']);
-
-//Crear
-Route::post('/createUser', [UsuariosController::class, 'createUser']);
-//Editar
-Route::patch('/editUser/{id}', [UsuariosController::class, 'patchUser']);
-//Eliminar/Deshabilitar
-Route::delete('/deleteUser/{id}', [UsuariosController::class, 'deleteUser']);
+   // Todas las rutas protegidas
+  /*   Route::get('/prueba', [UsuariosController::class, 'prueba']); */
+    
+    // Obtener todos
+    Route::get('/getAll', [UsuariosController::class, 'getAllUser']);// http://127.0.0.1:8000/api/v1/getAll
+    
+    // Obtener por id
+    Route::get('/user/{id}', [UsuariosController::class, 'usuarioById']); // http://127.0.0.1:8000/api/v1/user/id
+    // Obtener agrupado por fecha de creacion
+    Route::get('/getUsersGroupedByDate', [UsuariosController::class, 'getUsersGroupedByDate']); // http://127.0.0.1:8000/api/v1/getUsersGroupedByDate
+    
+    //Crear
+    Route::post('/createUser', [UsuariosController::class, 'createUser']); // http://127.0.0.1:8000/api/v1/createUser
+    //Editar
+    Route::patch('/editUser/{id}', [UsuariosController::class, 'patchUser']); // http://127.0.0.1:8000/api/v1/editUser/id
+    //Eliminar/Deshabilitar
+    Route::delete('/deleteUser/{id}', [UsuariosController::class, 'deleteUser']); // http://127.0.0.1:8000/api/v1/deleteUser/id
+});
 
 //REGISTRAR
-Route::post('/register', [UsuariosController::class, 'register']);
+Route::post('/register', [UsuariosController::class, 'register']); // http://127.0.0.1:8000/api/v1/register
 
 // LOGIN
-Route::post('/login', [UsuariosController::class, 'login']);
+Route::post('/login', [UsuariosController::class, 'login'])->name('login'); // http://127.0.0.1:8000/api/v1/login
+
+
