aa1a4ba6a838a65940fbb6107eea504a51eac9db
Mirna <mirnarecinos382@gmail.com>
2024-06-01 21:47:23 -0300

    add LoginRequest for validations

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Contracts\Validation\Validator;
+
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required', 
+        ];          
+    }
+
+    // Si se ingresa un campo no correspondiente
+    public function failedValidation(Validator $validator)
+    {
+        $response = response()->json([
+            'status' => false,
+            'message' => 'Validation error',
+            'errors' => $validator->errors()
+        ], 409);
+        throw new \Illuminate\Validation\ValidationException($validator, $response);
+  
+    }
+    
+}
